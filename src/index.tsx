35d63f807217652b4c42e02aeaffc6bbe73f3b62
Hannah Nordgren <hey@hereshannahs.info>
2019-12-22 22:50:04 -0800

    [WIP] initial directory setup and small bugfix in service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './Pages/App/App';
+import * as serviceWorker from './Utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
