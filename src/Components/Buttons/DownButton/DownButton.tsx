d2700dcb9580b40af12ef06bacd3713d12ce6fb2
Hannah Nordgren <hey@hereshannahs.info>
2019-12-31 18:38:56 -0500

    [FEATURE] added basic buttons, no style really

---
+++ src/Components/Buttons/DownButton/DownButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+	StyledDownButton,
+	DownButtonIcon,
+} from './DownButtonStyles';
+
+const DownButton: React.FC = () => {
+	return (
+		<StyledDownButton
+			onClick={() => console.log('down!')}
+		>
+			<DownButtonIcon />
+		</StyledDownButton>
+	);
+}
+
+export default DownButton;
