230ac922e860daefbb572d8637f7da2030167fb5
Hannah Nordgren <hey@hereshannahs.info>
2020-01-31 00:13:23 -0500

    [WIP] getting tests written

--- src/Redux/Reducers/rootReducer.test.ts
+++ src/Redux/Reducers/rootReducer.test.ts
@@ -1,24 +1,36 @@
 import React from 'react';
 import configureStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
 import { 
 	FETCH_PEOPLE_SUCCESS,
 	SORT_ITEMS_ASCENDING,
 	SORT_ITEMS_DESCENDING, 
 } from '../Actions/Actions'; 
-import { shallow } from 'enzyme';
-import { testArr } from '../../Utils/testingUtils';
+import { 
+	testArr,
+	testArrAsc,
+ } from '../../Utils/testingUtils';
 import { peopleReducer } from './rootreducer';
 
 describe('people reducer tests', () => {
+	const mockState = { people: []};
+
 	it('should return the initial state', () => {
-		// handle initial loading state
+		expect(peopleReducer(mockState, {})).toEqual({people: []})
 	});
+
 	it('should handle FETCH_PEOPLE_SUCCESS', () => {
-		// handle fetching people state
+		const successFetchState = {
+			type: FETCH_PEOPLE_SUCCESS,
+			people: testArr,
+		}
+		expect(peopleReducer(mockState, successFetchState)).toEqual({people: testArr})
 	});
+
 	it('should handle SORT_ITEMS_ASCENDING', () => {
 		// handle sorting ascending people state
 	});
+
 	it('should handle SORT_ITEMS_DESCENDING', () => {
 		// handle sorting descending people state
 	});
--- src/Utils/testingUtils.ts
+++ src/Utils/testingUtils.ts
@@ -18,3 +18,24 @@ export const testArr = [
       "edited": "12/20/2014",
     },
 ];
+
+export const testArrAsc = [
+     {
+      "name": "C-3PO",
+      "height": "167",
+      "mass": "75",
+      "eye_color": "yellow",
+      "hair_color": "n/a",
+      "created": "12/10/2014",
+      "edited": "12/20/2014",
+    },
+    {
+      "name": "Luke Skywalker",
+      "height": "172",
+      "mass": "77",
+      "eye_color": "blue",
+      "hair_color": "blond",
+      "created": "12/19/2014",
+      "edited": "12/20/2014",
+    },
+];
