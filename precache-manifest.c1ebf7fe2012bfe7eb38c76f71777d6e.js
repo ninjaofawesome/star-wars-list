6bf12792ce737fc21d8cb3b0c9fff33f9d876c26
Hannah Nordgren <hey@hereshannahs.info>
2020-02-01 18:34:45 -0500

    Updates

---
+++ precache-manifest.c1ebf7fe2012bfe7eb38c76f71777d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abc260dcae22643dd61df3bd76e455a",
+    "url": "/star-wars-list/index.html"
+  },
+  {
+    "revision": "34dcbd2ee119dacea995",
+    "url": "/star-wars-list/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "5f2fa776fdd0b032febb",
+    "url": "/star-wars-list/static/js/2.c807c2cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/star-wars-list/static/js/2.c807c2cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34dcbd2ee119dacea995",
+    "url": "/star-wars-list/static/js/main.bc12f800.chunk.js"
+  },
+  {
+    "revision": "94645cbf3b951b03194d",
+    "url": "/star-wars-list/static/js/runtime-main.f446e31e.js"
+  }
+]);
